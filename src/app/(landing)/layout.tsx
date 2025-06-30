5429679b88dcdbb63bbe17a121fa615a916a316b
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-30 21:21:50 +0100

    Refactor LandingLayout component to remove unused header and footer sections for cleaner layout

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,39 +1,11 @@
-import { Logo } from '@/components/custom/logo'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+import { Navigation } from '@/components/custom/navigation'
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen w-screen">
-      {/* <header className="sticky top-0 z-50 flex w-full items-center justify-between px-10 py-5 backdrop-blur-xs">
-        <Logo variant="dark" />
-      </header> */}
+      <Navigation />
 
-      <main className="h-full w-full bg-gray-200">
-        {children}
-
-        {/* <footer className="border-t bg-white">
-          <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-            <div className="text-center text-sm text-gray-500">
-              <p>
-                Powered by{' '}
-                <a
-                  href="https://haveibeenpwned.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-[#96A4D3] hover:underline"
-                >
-                  Have I Been Pwned
-                </a>{' '}
-                API
-              </p>
-              <p className="mt-2">
-                Sentinel helps you stay informed about password security and maintain good security practices.
-              </p>
-            </div>
-          </div>
-        </footer> */}
-      </main>
+      <main className="mx-auto flex h-full w-fit bg-gray-200">{children}</main>
     </div>
   )
 }

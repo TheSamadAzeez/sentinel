0ec41e8266f574221c39efaa4141b1d14f7aad88
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-30 22:58:58 +0100

    Refactor LandingLayout component for improved height styling

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ import { Navigation } from '@/components/custom/navigation'
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-full w-screen">
       <Navigation />
 
       <main className="flex w-full bg-gray-200 lg:mx-auto lg:h-fit lg:w-fit">{children}</main>
